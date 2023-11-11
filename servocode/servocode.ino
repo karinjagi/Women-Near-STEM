cc78e66f8455c651af242affb6e4d327bfb1296c
samanthaely <149627378+samanthaely@users.noreply.github.com>
2023-11-11 16:56:44 -0600

    Update servocode.ino

--- servocode/servocode.ino
+++ servocode/servocode.ino
@@ -1,13 +1,22 @@
 #include <Servo.h>
 Servo myservo;
 int pos = 0;
+const int buttonPin = 2;
 void setup() {
   // put your setup code here, to run once:
 myservo.attach(10)
-
+pinMode(buttonPin, INPUT);
+pinMode(myservo, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+buttonState = digitalRead(buttonPin);
+if (buttonState == HIGH) {
+  for (pos = 0; pos <= 180; pos += 1){
+    myservo.write(pos);
+  }
+if buttonState == LOW) {
+  for (pos = 180; pos >= 0; pos -= 1){
+    myservo.write(pos);}
 }
