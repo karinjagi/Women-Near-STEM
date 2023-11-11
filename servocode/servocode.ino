aabca7c7742b0d79afa0a0c43b3de95dbb4ccfa2
samanthaely <149627378+samanthaely@users.noreply.github.com>
2023-11-11 16:47:28 -0600

    Add files via upload

---
+++ servocode/servocode.ino
@@ -0,0 +1,13 @@
+#include <Servo.h>
+Servo myservo;
+int pos = 0;
+void setup() {
+  // put your setup code here, to run once:
+myservo.attach(10)
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
