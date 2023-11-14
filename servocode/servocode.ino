fd5d2ea07282f5b99abf96bac490cc339fc127d2
samanthaely <149627378+samanthaely@users.noreply.github.com>
2023-11-13 22:41:04 -0600

    Update servocode.ino

--- servocode/servocode.ino
+++ servocode/servocode.ino
@@ -5,6 +5,16 @@ const int buttonPin = 8;
 const int servoPin = 10;
 int buttonState = 0;
 int previousButtonState;
+int buttonState2 = 0;
+int previousButtonState2;
+const int buttonPin2 = 6;
+const int servoPin2 = 5;
+Servo myservo2;
+int buttonState3 = 0;
+int previousButtonState3;
+const int buttonPin3 = 11;
+const int servoPin3 = 3;
+Servo myservo3;
 
 unsigned long currentTime;
 unsigned long previousTime = 0;
@@ -15,12 +25,22 @@ void setup() {
   myservo.write(0);
   pinMode(buttonPin, INPUT);
   pinMode(servoPin, OUTPUT);
+  myservo2.attach(servoPin2);
+  myservo2.write(0);
+  pinMode(buttonPin2, INPUT);
+  pinMode(servoPin2, OUTPUT);
+  myservo3.attach(servoPin3);
+  myservo3.write(0);
+  pinMode(buttonPin3, INPUT);
+  pinMode(servoPin3, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //currentTime = millis();
   buttonState = digitalRead(buttonPin);
+  buttonState2 = digitalRead(buttonPin2);
+  buttonState3 = digitalRead(buttonPin3);
 
   if (previousButtonState != buttonState){
     if(buttonState == HIGH){
@@ -29,6 +49,21 @@ void loop() {
       myservo.write(0);
     }
   }
-
+  if (previousButtonState2 != buttonState2){
+    if(buttonState2 == HIGH){
+      myservo2.write(180);
+    } else {
+      myservo2.write(0);
+    }
+  }
+  if (previousButtonState3 != buttonState3){
+    if(buttonState3 == HIGH){
+      myservo3.write(180);
+    } else {
+      myservo3.write(0);
+    }
+  }
   previousButtonState = buttonState;
+  previousButtonState2 = buttonState2;
+  previousButtonState3 = buttonState3;
 }
